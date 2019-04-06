3499c07a57637710104bbedba2858eb87f9ed1f3
VaneOrtega <ortegatorresjocelynvanessa@gmail.com>
2019-04-06 00:09:46 -0600

    Cambio de nombre
    Cambio en el nombre del paquete

--- Hackaton_CORE/src/java/org/firecode/alcoholimetro/rest/RESTRegistro.java
+++ Hackaton_CORE/src/java/org/firecode/alcoholimetro/rest/RESTRegistro.java
@@ -1,3 +1,5 @@
+package org.firecode.alcoholimetro.rest;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
